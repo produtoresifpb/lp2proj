1934ea906651a84bfe50530aad4ba75ec419cc80
Mateus Mendes <bonitoclash609@gmail.com>
2024-03-24 20:08:12 -0300

    registro

--- routes/auth.js
+++ routes/auth.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/login', function(req, res, next) {
   res.render('login');
 });
+router.get('/registro', function(req, res, next) {
+  res.render('registro');
+});
 
 module.exports = router;
