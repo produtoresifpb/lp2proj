323a7f32c417234c7ef00e6f32fdabfef631b031
Danilo Cavalcante Beuttenmuller <danilocbjp@hotmail.com>
2024-07-13 22:03:17 -0300

    fix resetsenha link, edital date;

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,10 @@ var router = express.Router();
 
 router.post('/editais/create', function(req, res, next) { // cria um edital
     const date = new Date()
-    const creationDate = `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`
+    const day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+    const month = date.getMonth() < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1;
+    const year = date.getFullYear();
+    const creationDate = `${day}/${month}/${year}`;
     
     const edital = {
         description: req.body.description,
@@ -15,19 +18,13 @@ router.post('/editais/create', function(req, res, next) { // cria um edital
         id: uuid.v4()
     }
     // console.log(edital);
-
-    editais.push(edital) // bota no array
-
+    editais.push(edital); // bota no array
     res.status(201).render('sucess', {edital});
-
-    console.log('[ DEBUG ] Edital criado com sucesso. Identificador dele: ' + edital.id)
-    
+    // console.log('[ DEBUG ] Edital criado com sucesso. Identificador dele: ' + edital.id);
 });
 
 router.get('/editais/list', function(req, res, next) { // lista os editais
-    res.status(200).json({
-        editais
-    })
-})
+    res.status(200).json({editais});
+});
 
 module.exports = router;
