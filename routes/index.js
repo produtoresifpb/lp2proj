27dcd7c675690b3aba671bbc3471fe6b2578f3dc
Danilo Cavalcante Beuttenmuller <danilocbjp@hotmail.com>
2024-08-10 19:48:10 -0300

    fix notices not appearing on index page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const { getAllNotices } = require("../models/notice.js");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index");
+router.get("/", async function (req, res, next) {
+  try {
+    const editais = await getAllNotices("");
+    res.render("index", { editais: editais });
+  } catch (err) {
+    res.status(500).redirect("/", { error: err });
+  }
 });
 
 module.exports = router;
