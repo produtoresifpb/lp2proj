ab4f6b65046239f97dff85d64a746474ef17bbc4
Danilo Cavalcante Beuttenmuller <danilocbjp@hotmail.com>
2024-09-04 15:32:06 -0300

    fix filter

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const { getAllNotices } = require("../models/notice.js");
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
-    const editais = await getAllNotices("", {});
+    const editais = await getAllNotices("", "");
     res.render("index", { editais: editais });
 });
 
