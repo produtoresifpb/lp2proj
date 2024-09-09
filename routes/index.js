ecfd4fa1d3c942e2ee3568f3fe6ed2db6c4f3575
Faelson <135740581+MatheusFael@users.noreply.github.com>
2024-09-09 12:54:51 +0000

    problem with front and back solved

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,17 @@
 var express = require('express');
+const { getAllUsers } = require('../models/user')
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index');
 });
-router.get('/edital', function(req, res, next) {
+router.get('/edital', function (req, res, next) {
   res.render('edital');
 });
-router.get('/editais/create', function(req, res, next) {
-  res.render('create_notice');
+router.get('/editais/create', async function (req, res, next) {
+  const users = await getAllUsers();
+  res.render('create_notice', { users });
 });
 
 module.exports = router;
