6e54a3c6e6964d4ea27131116f46de7dfbfba145
VOTRON157 <bonitoclash609@gmail.com>
2024-09-25 10:26:09 -0300

    Co-authored-by: Faelson <MatheusFael@users.noreply.github.com> Co-authored-by: Danilo C Beuttenmuller <danilocb21@users.noreply.github.com> Co-authored-by: Ricardo Silva <RicardinhoGplays@users.noreply.github.com>

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const multer = require('multer');
+const upload = multer();
 const router = express.Router();
 const { createUser } = require("../models/user.js");
 
+
 /* Tive q desativar os layouts nesses pra tirar a navbar e o footer */
 router.get("/login", function (req, res, next) {
-  res.render("login", { layout: false });
+  res.render("auth/login", { layout: false });
 });
 
 router.post("/login", function (req, res, next) {
@@ -12,23 +15,35 @@ router.post("/login", function (req, res, next) {
 });
 
 router.get("/registro", function (req, res, next) {
-  res.render("registro", { layout: false });
-});
+  res.render("auth/registro", { layout: false });
+}); 
 
-router.post("/registro", async (req, res, next) => {
-  const user = req.body;
-  delete user.password2;
-  const createdUser = await createUser(user);
+router.post("/registro", upload.none(), async (req, res, next) => {
+  try {
+    const user = req.body;
+    delete user.password2;
+    console.log(user);
+    
+    const createdUser = await createUser(user);
+    
+    delete createUser.password;
+    delete createUser.cpf;
+    delete createUser.birthDate;
+    delete createUser.registerDate;
 
-  delete createUser.password;
-  delete createUser.cpf;
-  delete createUser.birthDate;
-  delete createUser.registerDate;
-  return res.json(createdUser);
+    return res.json(createdUser);
+  } catch (e) {
+    console.log(e)
+    if(e.code == 'P2002') {
+      res.status(400).json({
+        message: 'E-mail ou CPF já estão cadastrados.'
+       })
+    }
+  }
 });
 
 router.get("/resetsenha", function (req, res, next) {
-  res.render("recusenha", { layout: false });
+  res.render("auth/recusenha", { layout: false });
 });
 
 module.exports = router;
